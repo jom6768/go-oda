4cb820f7825a5726f7651b8e35554ac7cc7f96ce
Methee Suriyakrai <jom6768@gmail.com>
2024-11-15 16:31:39 +0700

    Completed_Except_BirthDate

--- oda/config/create_table.sql
+++ oda/config/create_table.sql
@@ -19,16 +19,73 @@ CREATE USER myuser WITH ENCRYPTED PASSWORD 'mypass';
 GRANT ALL PRIVILEGES ON DATABASE go_oda TO myuser;
 GRANT ALL ON SCHEMA public TO myuser;
 
--- TMF632_customer
+-- TMF632_Party Management API
 CREATE TABLE customer (
-    customer_id VARCHAR(50) PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    phone VARCHAR(20),
+    id VARCHAR(50) PRIMARY KEY NOT NULL,
+    gender VARCHAR(20),
+    countryOfBirth VARCHAR(50),
+    nationality VARCHAR(50),
+    maritalStatus VARCHAR(20),
+    birthDate TIMESTAMP,
+    givenName VARCHAR(50),
+    preferredGivenName VARCHAR(50),
+    familyName VARCHAR(50),
+    legalName VARCHAR(50), -- UNIQUE
+    middleName VARCHAR(50),
+    fullName VARCHAR(100),
+    formattedName VARCHAR(100),
     status VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO customer (id, gender, countryOfBirth, nationality, maritalStatus, birthDate, givenName, preferredGivenName, familyName, legalName, middleName, fullName, formattedName, status)
+VALUES ('42', 'female', 'United States', 'American', 'married', '1967-09-26T05:00:00.246Z', 'Jane', 'Lamborgizzia', 'Lamborgizzia', 'Smith', 'JL', 'Jane Smith ep Lamborgizzia', 'Jane Smith ep Lamborgizzia', 'validated') RETURNING id;
+
+SELECT id, gender, countryOfBirth, nationality, maritalStatus, birthDate, givenName, preferredGivenName, familyName, legalName, middleName, fullName, formattedName, status FROM customer;
+
+CREATE TABLE externalReference (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    externalIdentifierType VARCHAR(20),
+    type VARCHAR(20),
+    customer_id VARCHAR(50) REFERENCES customer (id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO externalReference (name, externalIdentifierType, type, customer_id) VALUES ('http://facebook.com/17263635', 'facebookId', 'ExternalIdentifier', '42');
+INSERT INTO externalReference (name, externalIdentifierType, type, customer_id) VALUES ('http://google.com/17263635', 'googleId', 'ExternalIdentifier', '42');
+INSERT INTO externalReference (name, externalIdentifierType, type, customer_id) VALUES ('http://facebook.com/17263635', 'facebookId', 'ExternalIdentifier', '44');
+
+SELECT name, externalIdentifierType, type FROM externalReference WHERE customer_id = '42';
+
+{
+  "id": "42",
+  "href": "https://serverRoot/tmf-api/party/v5/individual/42",
+  "@type": "Individual",
+  "@baseType": "Party",
+  "gender": "female",
+  "countryOfBirth": "United States",
+  "nationality": "American",
+  "maritalStatus": "married",
+  "birthDate": "1967-09-26T05:00:00.246Z",
+  "givenName": "Jane",
+  "preferredGivenName": "Lamborgizzia",
+  "familyName": "Lamborgizzia",
+  "legalName": "Smith",
+  "middleName": "JL",
+  "fullName": "Jane Smith ep Lamborgizzia",
+  "formattedName": "Jane Smith ep Lamborgizzia",
+  "status": "validated",
+  "externalReference": [
+    {
+      "name": "http://facebook.com/17263635",
+      "externalIdentifierType": "facebookId",
+      "@type": "ExternalIdentifier"
+    }
+  ]
+}
 
 -- Drop Table
 DROP TABLE customer;
--- oda/tmf632/main.go
+++ oda/tmf632/main.go
@@ -12,11 +12,30 @@ import (
 )
 
 type Customer struct {
-	ID     string `json:"customer_id" binding:"required"`
-	Name   string `json:"name" binding:"required"`
-	Email  string `json:"email" binding:"email"`
-	Phone  string `json:"phone"`
-	Status string `json:"status"`
+	ID                 string               `json:"id" binding:"required"`
+	Href               string               `json:"href"`
+	Type               string               `json:"@type"`
+	BaseType           string               `json:"@baseType"`
+	Gender             string               `json:"gender,omitempty"`
+	CountryOfBirth     string               `json:"countryOfBirth,omitempty"`
+	Nationality        string               `json:"nationality,omitempty"`
+	MaritalStatus      string               `json:"maritalStatus,omitempty"`
+	BirthDate          string               `json:"birthDate,omitempty"`
+	GivenName          string               `json:"givenName,omitempty"`
+	PreferredGivenName string               `json:"preferredGivenName,omitempty"`
+	FamilyName         string               `json:"familyName,omitempty"`
+	LegalName          string               `json:"legalName,omitempty"`
+	MiddleName         string               `json:"middleName,omitempty"`
+	FullName           string               `json:"fullName" binding:"required"`
+	FormattedName      string               `json:"formattedName,omitempty"`
+	Status             string               `json:"status,omitempty"`
+	ExternalReferences *[]ExternalReference `json:"externalReference,omitempty"`
+}
+
+type ExternalReference struct {
+	Name                   string `json:"id" binding:"required"`
+	ExternalIdentifierType string `json:"externalIdentifierType"`
+	Type                   string `json:"@type"`
 }
 
 var db *sql.DB
@@ -49,24 +68,79 @@ func createCustomer(c *gin.Context) {
 		return
 	}
 
-	query := `INSERT INTO customer (customer_id, name, email, phone, status) VALUES ($1, $2, $3, $4, $5) RETURNING customer_id`
-	err := db.QueryRow(query, newCustomer.ID, newCustomer.Name, newCustomer.Email, newCustomer.Phone, "Active").Scan(&newCustomer.ID)
+	query := `INSERT INTO customer (id, gender, countryOfBirth, nationality, maritalStatus, birthDate, givenName, preferredGivenName, familyName, legalName, middleName, fullName, formattedName, status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING id`
+	err := db.QueryRow(query, newCustomer.ID, newCustomer.Gender, newCustomer.CountryOfBirth, newCustomer.Nationality, newCustomer.MaritalStatus, newCustomer.BirthDate, newCustomer.GivenName, newCustomer.PreferredGivenName, newCustomer.FamilyName, newCustomer.LegalName, newCustomer.MiddleName, newCustomer.FullName, newCustomer.FormattedName, newCustomer.Status).Scan(&newCustomer.ID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert customer" + err.Error()})
 		return
 	}
 
+	if newCustomer.ExternalReferences != nil {
+		for _, externalReference := range *newCustomer.ExternalReferences {
+			var id int
+			query = `INSERT INTO externalReference (name, externalIdentifierType, type, customer_id) VALUES ($1, $2, $3, $4) RETURNING id`
+			err := db.QueryRow(query, externalReference.Name, externalReference.ExternalIdentifierType, externalReference.Type, newCustomer.ID).Scan(&id)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert externalReference" + err.Error()})
+				return
+			}
+		}
+	}
+
 	c.JSON(http.StatusCreated, newCustomer)
 }
 
-// getCustomer retrieves a customer by ID
+// getCustomers retrieves a customer
+func getCustomers(c *gin.Context) {
+	var customers []Customer
+
+	query := `SELECT id, gender, countryOfBirth, nationality, maritalStatus, birthDate, givenName, preferredGivenName, familyName, legalName, middleName, fullName, formattedName, status FROM customer`
+	rows, err := db.Query(query)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve customers"})
+		return
+	}
+	defer rows.Close()
+
+	// Iterate over the result set and populate the slice
+	for rows.Next() {
+		var customer Customer
+		if err := rows.Scan(&customer.ID, &customer.Gender, &customer.CountryOfBirth, &customer.Nationality, &customer.MaritalStatus, &customer.BirthDate, &customer.GivenName, &customer.PreferredGivenName, &customer.FamilyName, &customer.LegalName, &customer.MiddleName, &customer.FullName, &customer.FormattedName, &customer.Status); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan customer"})
+			return
+		}
+
+		// Set the custom field
+		customer.Href = "https://serverRoot/tmf-api/party/v5/individual/" + customer.ID
+		customer.Type = "Individual"
+		customer.BaseType = "Party"
+
+		if errMsg := getExternalReference(&customer); errMsg != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+			return
+		}
+
+		// Append to the customers slice
+		customers = append(customers, customer)
+	}
+
+	// Check for errors encountered during iteration
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while fetching customers"})
+		return
+	}
+
+	c.JSON(http.StatusOK, customers)
+}
+
+// getCustomerById retrieves a customer by ID
 func getCustomerById(c *gin.Context) {
 	id := c.Param("id")
 	var customer Customer
 
-	query := `SELECT customer_id, name, email, phone, status FROM customer WHERE customer_id = $1 LIMIT 1`
+	query := `SELECT id, gender, countryOfBirth, nationality, maritalStatus, birthDate, givenName, preferredGivenName, familyName, legalName, middleName, fullName, formattedName, status FROM customer WHERE id = $1 LIMIT 1`
 	row := db.QueryRow(query, id)
-	if err := row.Scan(&customer.ID, &customer.Name, &customer.Email, &customer.Phone, &customer.Status); err != nil {
+	if err := row.Scan(&customer.ID, &customer.Gender, &customer.CountryOfBirth, &customer.Nationality, &customer.MaritalStatus, &customer.BirthDate, &customer.GivenName, &customer.PreferredGivenName, &customer.FamilyName, &customer.LegalName, &customer.MiddleName, &customer.FullName, &customer.FormattedName, &customer.Status); err != nil {
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
 			return
@@ -75,25 +149,79 @@ func getCustomerById(c *gin.Context) {
 		return
 	}
 
+	// Set the custom field
+	customer.Href = "https://serverRoot/tmf-api/party/v5/individual/" + customer.ID
+	customer.Type = "Individual"
+	customer.BaseType = "Party"
+
+	if errMsg := getExternalReference(&customer); errMsg != "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": errMsg})
+		return
+	}
+
 	c.JSON(http.StatusOK, customer)
 }
 
-// deleteCustomer deletes a customer by ID
+// deleteCustomerById deletes a customer by ID
 func deleteCustomerById(c *gin.Context) {
 	id := c.Param("id")
 
-	query := `DELETE FROM customer WHERE customer_id = $1`
+	query := `DELETE FROM externalReference WHERE customer_id = $1`
 	res, err := db.Exec(query, id)
 	if err == nil {
 		count, err := res.RowsAffected()
 		if err == nil {
+			if count >= 1 {
+				c.JSON(http.StatusOK, fmt.Sprintf("Delete externalReferences of customer: %s completed\n", id))
+			}
+		}
+	}
+	log.Println("no externalReferences of customer:", id)
+
+	query = `DELETE FROM customer WHERE id = $1`
+	res, err = db.Exec(query, id)
+	if err == nil {
+		count, err := res.RowsAffected()
+		if err == nil {
 			if count == 1 {
-				c.JSON(http.StatusOK, fmt.Sprintf("customer_id = %s completed.", id))
+				c.JSON(http.StatusOK, fmt.Sprintf("id = %s completed.", id))
 				return
 			}
 		}
 	}
 	c.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
+}
+
+func getExternalReference(customer *Customer) string {
+	var externalReferences []ExternalReference
+
+	query := `SELECT name, externalIdentifierType, type FROM externalReference WHERE customer_id = $1`
+	rows, err := db.Query(query, customer.ID)
+	if err != nil {
+		return "Failed to retrieve externalReferences"
+	}
+	defer rows.Close()
+
+	// Iterate over the result set and populate the slice
+	for rows.Next() {
+		var externalReference ExternalReference
+		if err := rows.Scan(&externalReference.Name, &externalReference.ExternalIdentifierType, &externalReference.Type); err != nil {
+			return "Failed to scan externalReference"
+		}
+
+		// Append to the externalReferences slice
+		externalReferences = append(externalReferences, externalReference)
+	}
+
+	// Check for errors encountered during iteration
+	if err = rows.Err(); err != nil {
+		return "Error while fetching externalReferences"
+	}
+
+	if externalReferences != nil {
+		customer.ExternalReferences = &externalReferences
+	}
+	return ""
 }
 
 func main() {
@@ -102,6 +230,7 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/tmf632/customer", createCustomer)
+	r.GET("/tmf632/customers", getCustomers)
 	r.GET("/tmf632/customer/:id", getCustomerById)
 	r.DELETE("/tmf632/customer/:id", deleteCustomerById)
 	r.Run(":8081")
